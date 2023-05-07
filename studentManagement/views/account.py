ba5bcdffa0b8a7fd7fb5a4bdab6fae031215c14c
shoetify <shoetify@gmail.com>
2023-05-07 21:04:32 +0800

    4-7 login success add session

--- studentManagement/views/account.py
+++ studentManagement/views/account.py
@@ -3,19 +3,22 @@ from django import forms
 from studentManagement import models
 from studentManagement.utils.bootstrap import BootStrapForm
 from studentManagement.utils.encrypt import md5
+from django.shortcuts import redirect
+
 
 # Form 组件的写法
 class LoginForm(BootStrapForm):
     username = forms.CharField(
         label="用户名",
         widget=forms.TextInput(attrs={'class': 'form-control'}),
-        required=True,  #表示该项不能为空
+        required=True,  # 表示该项不能为空
     )
     password = forms.CharField(
         label="密码",
         widget=forms.PasswordInput(attrs={'class': 'form-control'}),
         required=True,  # 表示该项不能为空
     )
+
     def clean_password(self):
         # 将密码的明文转变为密文
         pwd = self.cleaned_data.get('password')
@@ -27,6 +30,7 @@ class LoginModelForm(forms.ModelForm):
     class Meta:
         models = models.Admin
         field = ['username', 'password']
+
 
 def login(request):
     """登录"""
@@ -40,9 +44,17 @@ def login(request):
         # 验证成功，获取到的用户名和密码
 
         # 数据库校验用户名和密码是否正确，获取用户对象 或者 None
-        admin_object = models.Admin.objects.filter(**form.cleaned_data).first()  #将数据文件（格式为字典）作为语句查询数据库
+        admin_object = models.Admin.objects.filter(**form.cleaned_data).first()  # 将数据文件（格式为字典）作为语句查询数据库
+
+        # 如果用户名或者密码错误
         if not admin_object:
-            form.add_error("password", "用户名或密码错误")   #主动添加错误信息在password栏显示错误信息
+            form.add_error("password", "用户名或密码错误")  # 主动添加错误信息在password栏显示错误信息
             return render(request, 'login.html', {'form': form})
+
+        # 如果用户名和密码正确
+        # 网站需要生成一个随机字符串； 并将‘info'信息写到用户浏览器的cookie中； 再写入到session中；
+        request.session["info"] = {'id': admin_object.id, 'name': admin_object.username}
+
+        return redirect("/admin/list")
 
     return render(request, 'login.html', {'form': form})
