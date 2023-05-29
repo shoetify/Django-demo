e991794ebe7933f983f26249d5bb86e1e26d0961
shoetify <shoetify@gmail.com>
2023-05-30 00:15:20 +0800

    20230529 5-6 Order Edit by Ajax

--- studentManagement/views/order.py
+++ studentManagement/views/order.py
@@ -23,7 +23,7 @@ def order_list(request):
     """订单列表"""
 
     queryset = models.Order.objects.all().order_by('-id')
-    page_object = Pagination(request,queryset)
+    page_object = Pagination(request, queryset)
     form = OrderModelForm()
 
     context = {
@@ -31,8 +31,6 @@ def order_list(request):
         'queryset': page_object.page_queryset,
         "page_string": page_object.html(),
     }
-
-
 
     return render(request, "order_list.html", context)
 
@@ -53,9 +51,25 @@ def order_add(request):
 
     return JsonResponse({"status": False, 'error': form.errors})
 
+
 def order_delete(request):
     """删除订单"""
     uid = request.GET.get('uid')
     print(uid)
     models.Order.objects.filter(id=uid).delete()
     return JsonResponse({"status": True})
+
+
+def order_detail(request):
+    """根据ID获取订单数据"""
+    uid = request.GET.get("uid")
+    row_dict = models.Order.objects.filter(id=uid).values("title", "price", "status").first()
+    if not row_dict:
+        return JsonResponse({"status": False, 'error': "数据不存在。"})
+
+    result = {
+        "status": True,
+        "data": row_dict,
+    }
+
+    return JsonResponse({"status": True, "data": result})
